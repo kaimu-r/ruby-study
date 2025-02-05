d39ed0b4964e77b64354d0066b33e5baf1cea9a3
kaimu-r <kaimu.seino@rizapgroup.com>
2025-02-05 11:24:24 +0900

    条件分岐や論理演算子、繰り返し処理について学習した

---
+++ chapter03/3-5/problem08.rb
@@ -0,0 +1,8 @@
+season = "春"
+
+case season
+when "春"
+  puts "かき氷かってこう!"
+else
+  puts "あんまんかってこう!"
+end
---
+++ chapter03/3-6/problem09.rb
@@ -0,0 +1,10 @@
+7.times do |i|
+  case i
+  when 0, 3
+    puts "カフェラテ"
+  when 6
+    puts "フラペチーノ"
+  else
+    puts "モカ"
+  end
+end
